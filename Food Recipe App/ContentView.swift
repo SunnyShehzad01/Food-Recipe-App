3ad9034c368c491dcfdd9b4610c2cc45c7a4d912
Shehzad khan <khan415072@gmail.com>
2022-04-02 17:22:03 +0530

    Initial Commit

---
+++ Food Recipe App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Recipe App
+//
+//  Created by Sunny Shehzad on 02/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food Recipe App/Food_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Recipe_AppApp.swift
+//  Food Recipe App
+//
+//  Created by Sunny Shehzad on 02/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Food_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
